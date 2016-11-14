3ce43313b1b7630874abc353a77ad5506ba8d7ff
Giuseppe Peronato <giuseppe.peronato@epfl.ch>
2016-11-14 13:11:56 +0100

    Run input to LoadResults
    Added a "Run" boolean input to the LoadResults component;
    moved most of the code to functions

--- src/Honeybee_CitySim-LoadResults.py
+++ src/Honeybee_CitySim-LoadResults.py
@@ -52,80 +52,88 @@ def list_to_tree(input, none_and_holes=True, source=[0]):
                 if none_and_holes: tree.Insert(item,path,i)
                 elif item is not None: tree.Add(item,path)
     if input is not None: t=Tree[object]();proc(input,t,source[:]);return t
-
-
-#Load SW output file
-in_file = open(path+name+"_SW.out","r")
-txt = in_file.read()
-in_file.close()
-
-results = txt.splitlines()
-header = results[0]
-results.pop(0) #remove header
-
-        
-#Parse header
-import re
-header = header.split()
-#header.pop(0)
-bIDs = []
-sIDs = []
-for i in range(len(header)):
-    if i == 0:
-       bIDs.append('')
-       sIDs.append('')
-    else:
-        b = header[i].split('(')
-        s = re.search('(?<=:)\w+', header[i])
-        b = b[0]
-        s = s.group(0)
-        #print 'building', b, 'surface', s
-        bIDs.append(b)
-        sIDs.append(int(s))
-
-
-#Parse results
-irrH = []
-
-for l in results: #for each line corresponding to a hour
-    bldg = []
-    isrf = []
-    for i in l.split(): #split the surfaces
-        isrf.append(float(i))
-    irrH.append(isrf)
-print len(isrf)
-irrS = []
-for s in xrange(len(sIDs)): #for each surface
-    srf = []
-    for h in range(len(irrH)): #for each hour
-        srf.append(irrH[h][s])
-    irrS.append(srf)
     
-#Remove terrain from results 
-bIDs2 = []
-sIDs2 = []
-IDs = []
-irrS2 = []
-for s in xrange(len(bIDs)):
-    if bIDs[s] != "NA" and bIDs[s] != "" : #remove columns
-       sIDs2.append(int(sIDs[s]))
-       bIDs2.append(int(bIDs[s]))
-       IDs.append([int(bIDs[s]),int(sIDs[s])])
-       irrS2.append(irrS[s])
-
-
     
-bIDs2set = set(bIDs2) #create a set of unique building IDs
-#print len(irrS2[0])
+def loadOut(path,name,type="SW"):
+    #Load SW output file
+    in_file = open(path+name+"_"+type+".out","r")
+    txt = in_file.read()
+    in_file.close()
+    results = txt.splitlines()
+    header = results[0]
+    results.pop(0) #remove header
+    return header, results
 
-output = []
-for b in bIDs2set:
-    bldg = []
-    for s in xrange(len(sIDs2)):
-        if bIDs2[s] == b:
-            #print s, sIDs2[s]
-            bldg.append(irrS2[sIDs2[s]]) #Reorder surfaces
-    output.append(bldg)  
+def parseHead(head):        
+    #Parse header
+    import re
+    header = head.split()
+    #header.pop(0)
+    bIDs = []
+    sIDs = []
+    for i in range(len(header)):
+        if i == 0:
+            bIDs.append('')
+            sIDs.append('')
+        else:
+            b = header[i].split('(')
+            s = re.search('(?<=:)\w+', header[i])
+            b = b[0]
+            s = s.group(0)
+            #print 'building', b, 'surface', s
+            bIDs.append(b)
+            sIDs.append(int(s))
+    return bIDs, sIDs
+
+def parseRes(results):
+    #Parse results
+    irrH = []
+
+    for l in results: #for each line corresponding to a hour
+        bldg = []
+        isrf = []
+        for i in l.split(): #split the surfaces
+            isrf.append(float(i))
+        irrH.append(isrf)
+    #print len(isrf)
+    irrS = []
+    for s in xrange(len(sIDs)): #for each surface
+        srf = []
+        for h in range(len(irrH)): #for each hour
+            srf.append(irrH[h][s])
+        irrS.append(srf)
+    return irrS
+
+def removeTerr(irrS,bIDs,sIDs):   
+    #Remove terrain from results 
+    bIDs2 = []
+    sIDs2 = []
+    IDs = []
+    irrS2 = []
+    for s in xrange(len(bIDs)):
+        if bIDs[s] != "NA" and bIDs[s] != "" : #remove columns
+            sIDs2.append(int(sIDs[s]))
+            bIDs2.append(int(bIDs[s]))
+            IDs.append([int(bIDs[s]),int(sIDs[s])])
+            irrS2.append(irrS[s])
+
+
+if Run:
+    header, results = loadOut(path,name,"SW")
+    parseHead(header)
+    parseRes(results)
+    removeTerr(irrS,bIDs,sIDs)
+    bIDs2set = set(bIDs2) #create a set of unique building IDs
+    #print len(irrS2[0])
+
+    output = []
+    for b in bIDs2set:
+        bldg = []
+        for s in xrange(len(sIDs2)):
+            if bIDs2[s] == b:
+                #print s, sIDs2[s]
+                bldg.append(irrS2[sIDs2[s]]) #Reorder surfaces
+        output.append(bldg)  
 
   
-SW = list_to_tree(output,none_and_holes=True, source=[])
+    SW = list_to_tree(output,none_and_holes=True, source=[])
